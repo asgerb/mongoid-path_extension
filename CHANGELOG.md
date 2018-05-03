bcea7a100c9ef6eac7ab952d87cc2a245ac3d471
Tomáš Celizna <tomas.celizna@gmail.com>
2018-05-03 09:34:04 +0200

    0.1.2

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# CHANGELOG
+
+## 0.1.2
+
+* Mongoid 7 compatibility
--- lib/mongoid/path_extension/version.rb
+++ lib/mongoid/path_extension/version.rb
@@ -1,5 +1,5 @@
 module Mongoid
   class PathExtension < String
-    VERSION = '0.1.1'.freeze
+    VERSION = '0.1.2'.freeze
   end
 end
